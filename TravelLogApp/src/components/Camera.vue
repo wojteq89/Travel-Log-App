de86f58ded043a854a9e83535d017a1156723db3
Wojciech Męczyński <wojteq898@gmail.com>
2025-01-14 20:28:01 +0100

    Improve more styles & add toast

--- TravelLogApp/src/components/Camera.vue
+++ TravelLogApp/src/components/Camera.vue
@@ -1,9 +1,15 @@
 <template>
+  <head>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+</head>
   <div class="main-container">
       <button class="take-photo-button" v-if="!photo" @click="takePhoto">
           <i class="fas fa-camera"></i>
           <br> Take a photo
       </button>
+      <div v-if="showToast" class="toast">
+        Photo saved successfully!
+      </div>
 
       <div v-if="photo" class="photo-details">
         <button class="save-button" @click="savePhoto">
@@ -11,63 +17,65 @@
         <input class="inputs" v-model="photoName" placeholder="Enter photo name" />
         <input class="inputs" v-model="photoDescription" placeholder="Enter photo description" />
       </div>
+
+      <!-- Custom Toast Notification -->
   </div>
 </template>
 
-  
 <script>
-import {
-    Camera,
-    CameraResultType,
-    CameraSource
-} from '@capacitor/camera';
-import {
-    mapActions
-} from 'vuex';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+import { mapActions } from 'vuex';
 
 export default {
-    name: 'Camera',
-    data() {
-        return {
-            photo: null, // URL zdjęcia
-            photoName: '', // Nazwa zdjęcia
-            photoDescription: '', // Opis zdjęcia
-        };
-    },
-    methods: {
-        ...mapActions(['addPhoto']),
-        async takePhoto() {
-            try {
-                const result = await Camera.getPhoto({
-                    resultType: CameraResultType.Uri,
-                    source: CameraSource.Camera,
-                    quality: 100,
-                });
+  name: 'Camera',
+  data() {
+    return {
+      photo: null, // URL zdjęcia
+      photoName: '', // Nazwa zdjęcia
+      photoDescription: '', // Opis zdjęcia
+      showToast: false, // Flaga do kontrolowania widoczności toastu
+    };
+  },
+  methods: {
+    ...mapActions(['addPhoto']),
+    async takePhoto() {
+      try {
+        const result = await Camera.getPhoto({
+          resultType: CameraResultType.Uri,
+          source: CameraSource.Camera,
+          quality: 100,
+        });
 
-                this.photo = result.webPath;
-            } catch (error) {
-                console.error('Error taking photo:', error);
-            }
-        },
-        savePhoto() {
-            const newPhoto = {
-                url: this.photo,
-                name: this.photoName,
-                description: this.photoDescription,
-            };
-
-            this.addPhoto(newPhoto);
-            this.photo = null; // Resetowanie zdjęcia po zapisaniu
-            this.photoName = ''; // Resetowanie nazwy
-            this.photoDescription = ''; // Resetowanie opisu
-        },
+        this.photo = result.webPath;
+      } catch (error) {
+        console.error('Error taking photo:', error);
+      }
     },
+    savePhoto() {
+      const newPhoto = {
+        url: this.photo,
+        name: this.photoName,
+        description: this.photoDescription,
+      };
+
+      this.addPhoto(newPhoto);
+      this.photo = null; // Resetowanie zdjęcia po zapisaniu
+      this.photoName = ''; // Resetowanie nazwy
+      this.photoDescription = ''; // Resetowanie opisu
+
+      // Show the toast notification
+      this.showToast = true;
+
+      // Hide toast after 2 seconds
+      setTimeout(() => {
+        this.showToast = false;
+      }, 2000);
+    },
+  },
 };
 </script>
 
-  
 <style scoped>
-
 .main-container {
   width: 100%;
   height: 100%;
@@ -77,15 +85,15 @@ export default {
 }
 
 button {
-    padding: 20px;
-    border: 1px solid transparent;
-    border-radius: 50%;
-    width: 200px;
-    height: 200px;
-    cursor: pointer;
-    box-shadow: 10px 10px 100px rgba(255, 255, 255, 0.1);
-    transition: all 0.3s ease-in-out;
-    color: rgb(202, 202, 202);
+  padding: 20px;
+  border: 1px solid transparent;
+  border-radius: 50%;
+  width: 200px;
+  height: 200px;
+  cursor: pointer;
+  box-shadow: 10px 10px 100px rgba(255, 255, 255, 0.1);
+  transition: all 0.3s ease-in-out;
+  color: rgb(202, 202, 202);
 }
 
 button:hover {
@@ -94,7 +102,6 @@ button:hover {
   transform: scale(1.1);
   box-shadow: 10px 10px 100px rgba(255, 255, 255, 0.356);
 }
-
 
 .photo-details {
   width: 100%;
@@ -106,27 +113,39 @@ button:hover {
 }
 
 i {
-    font-size: 100px;
+  font-size: 100px;
 }
 
-.inputs{
-    width: 25%;
-    height: 50px;
-    padding: 20px;
-    text-align: center;
-    border-radius: 20px;
-    border: 1px solid grey;
-    transition: all 0.3s ease-in-out;
-  }
+.inputs {
+  width: 25%;
+  height: 50px;
+  padding: 20px;
+  text-align: center;
+  border-radius: 20px;
+  border: 1px solid grey;
+  transition: all 0.3s ease-in-out;
+}
 
 .inputs:focus {
-    width: 80%;
-    text-align: left;
+  width: 80%;
 }
 
 @media only screen and (max-width: 600px) {
   .inputs {
     width: 50%;
   }
+}
+
+/* Custom Toast Styles */
+.toast {
+  position: absolute;
+  bottom: 70px;
+  left: 20px;
+  background-color: #333;
+  color: #fff;
+  padding: 15px;
+  border-radius: 5px;
+  font-size: 16px;
+  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.2);
 }
 </style>
